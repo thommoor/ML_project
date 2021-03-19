5d07ec9158328d7256ff7e2e6fd016137d2ab2c8
thommoor <thommoor@iu.edu>
2021-03-19 12:40:10 -0400

    project files

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import jsonify
+import connexion
+from joblib import load
+
+#load the model
+
+my_model = load('svc_model.pkl')
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
+
+# Read the yaml file to configure the endpoints
+app.add_api("master.yaml")
+
+# create a URL route in our application for "/"
+@app.route("/")
+def home():
+    msg = {"msg": "It's working!"}
+    return jsonify(msg)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
